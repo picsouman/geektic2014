f61b9263d1e7e2043cd92789aa25ca6c26d5d154
picsouman <aimeric.01@hotmail.fr>
2015-06-29 17:13:55 +0200

    première vue affichée (manque les lien) le css est en cours...

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,21 @@
 var app = angular.module("geektic", ['ngRoute']);
 
+// CONTROLLERS
 app.controller('HelloCtrl', function($scope, $http) {
     $http.get('/api/hello').success(function(helloMessage) {
         $scope.helloMessage = helloMessage;
     });
 });
+
+app.controller('userList', function($scope, $http) {
+	$http.get('/api/search').success(function(result) {
+		$scope.userList = result;
+	});
+});
+
+
+// PARTIALS
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {	controller : "userList",
+    							templateUrl : 'partials/userList.html'});
+}])
